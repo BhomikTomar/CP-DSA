b11c4b7309d7cb2c874e19282b07afc0bee45753
BhomikTomar <bhomiktomar@gmail.com>
2024-10-06 18:43:33 +0530

    added permutation_swap c++ solution

---
+++ 900/permutation_swap.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n+1);
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+
+        int ans = 0;
+        for(int i=1;i<n;i++){
+            ans = __gcd(abs(arr[i]-i),ans);
+        }
+
+        cout<<ans<<endl;
+    }
+    return 0;
+}
