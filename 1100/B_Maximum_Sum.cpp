e99aebb86c59a8e2c299f3e72fd281e56e8d6b32
BhomikTomar <bhomiktomar@gmail.com>
2024-11-30 22:48:21 +0530

    added maximum_sum cpp solution

---
+++ 1100/B_Maximum_Sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        vector<long long> arr(n), pre(n+1);
+
+        for (long long i = 0; i < n; i++) {
+            cin >> arr[i]; 
+        }
+
+        long long maxi = 0;
+
+        sort(arr.begin(), arr.end());
+
+        for (long long i = 0; i < n; i++) {
+            pre[i+1] = pre[i] + arr[i];
+        }
+
+        for (long long i = 0; i <= k; i++) {
+            maxi = max(maxi, pre[n-i] - pre[2*(k-i)]);
+        }
+
+        cout << maxi << endl;
+    }
+
+    return 0;
+}
